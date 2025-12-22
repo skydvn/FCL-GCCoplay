956bba1ca7f46e251e2b559c98643a7913b0bf6f
Jackson2706 <dung27062002@gmail.com>
2025-12-22 09:58:34 +0700

    update scripts

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 python3 system/main.py --cfp ./hparams/cifar100/Ours_v2_cifar100.json --wandb True --offlog True --log True --note final
-python3 system/main.py --cfp ./hparams/cifar100/Ours_cifar100.json --wandb True --offlog True --log True --note final
+# python3 system/main.py --cfp ./hparams/cifar100/Ours_cifar100.json --wandb True --offlog True --log True --note final
 python3 system/main.py --cfp ./hparams/cifar100/FedAvg_cifar100.json --wandb True --offlog True --log True --note final
